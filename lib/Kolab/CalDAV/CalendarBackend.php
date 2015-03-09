6c947ece4a3aeadfd821d7f204cf95f4f8f43880
Thomas Bruederli <bruederli@kolabsys.com>
2015-03-09 12:56:24 +0100

    Handle resources with multiple occurrences of the same event according to RFC 4791 (#4733)
    Attention: this requires commit 83d3d01 from roundcubemail-plugins-kolab.

--- lib/Kolab/CalDAV/CalendarBackend.php
+++ lib/Kolab/CalDAV/CalendarBackend.php
@@ -1013,7 +1013,14 @@ class CalendarBackend extends CalDAV\Backend\AbstractBackend implements CalDAV\B
             };
         }
 
-        return $ical->export(array($event), null, false, $get_attachment, false);
+        $events = array($event);
+
+        // add more instances from exceptions (not recurrence) to the output
+        if (!empty($event['exceptions']) && empty($event['recurrence'])) {
+            $events = array_merge($events, $event['exceptions']);
+        }
+
+        return $ical->export($events, null, false, $get_attachment, false);
     }
 
     /**
