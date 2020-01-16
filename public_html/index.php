7c04396b40b3b0294a41dcf858d1cbc9745f6b93
Jeroen van Meeuwen <kanarip@kanarip.ch>
2020-01-16 11:46:50 +0100

    Bump version number

--- public_html/index.php
+++ public_html/index.php
@@ -26,7 +26,7 @@
 
 // define some environment variables used throughout the app and libraries
 define('KOLAB_DAV_ROOT', realpath('../'));
-define('KOLAB_DAV_VERSION', '0.4.2');
+define('KOLAB_DAV_VERSION', '0.4.3');
 define('KOLAB_DAV_START', microtime(true));
 
 define('RCUBE_INSTALL_PATH', KOLAB_DAV_ROOT . '/');
